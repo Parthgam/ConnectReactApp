e25ccffdcc94be7d380d15e93b1a9e6229880e6c
Parthgam <parth.bsr@gmail.com>
2019-07-31 01:57:30 +0530

    Adding Models

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,61 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-//@route    GET api/users
+// Getting User Model
+const User = require('../../models/User');
+
+//@route    POST api/users
 //@desc     TEST Route
 //@access   Public
-router.get('/', (req, res) => res.send('User Route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid Email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        res.status(400).json({ errors: [{ msg: 'User already exists.' }] });
+      }
+      // Get Users Gravatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      // Encrypt Passwords
+
+      // Return JsonWebTokens
+    } catch (err) {}
+
+    res.send('User Route');
+  }
+);
 
 module.exports = router;
