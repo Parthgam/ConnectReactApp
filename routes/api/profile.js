9a4281b4bd6b69294be81a36a2bf22b4df5d0542
Parthgam <parth.bsr@gmail.com>
2019-08-21 00:42:17 +0530

    Get All Profiles and Profile by User ID

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -108,4 +108,41 @@ router.post(
     }
   }
 );
+
+//@route    GET api/profile
+//@desc     GET all profiles
+//@access   Public
+
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    console.log('Hello');
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('server error');
+  }
+});
+
+//@route    GET api/profile/user/:user_id
+//@desc     GET Profile by user ID
+//@access   Public
+
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) return res.status(400).json({ msg: 'Profile not Found' });
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not Found' });
+    }
+    res.status(500).send('server error');
+  }
+});
+
 module.exports = router;
